801fb10a876848f07c571d78a0141894b1b55e9c
sleepingchris <chris92wang@gmail.com>
2019-06-01 11:53:38 -0400

    updated template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root1',
+  selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  name = 'chris';
 }
