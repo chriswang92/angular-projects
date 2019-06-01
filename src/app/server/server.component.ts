a0eae3e25d682add2201f86da6cebc167ad4289f
sleepingchris <chris92wang@gmail.com>
2019-06-01 16:23:50 -0400

    basics

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-server',
+    templateUrl: './server.component.html',
+    styles: [`
+        h3: {
+            color : dodger-blue
+        }
+    `]
+})
+export class ServerComponent{
+
+}
